40e259b1462af044ed37bae26ad5feee7000e38f
Bhagath Vaishnav P <84322745+Bhagathvp@users.noreply.github.com>
2023-10-17 14:46:42 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const photographerRoute = require("./routes/photographerRoute");
 const chatRoute = require('./routes/chatRouter')
 
 app.use(cors({
-    origin:"hinged-client.vercel.app",
+    origin:"https://hinged.live",
     
 }))
 
