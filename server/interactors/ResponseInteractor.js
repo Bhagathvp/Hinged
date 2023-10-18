729c985e2efc8f2d7806f98dd049084bd672d279
Bhagath Vaishnav P <bhagathvp.ramu@gmail.com>
2023-10-19 02:56:48 +0530

    bug fixing in api

--- server/interactors/ResponseInteractor.js
+++ server/interactors/ResponseInteractor.js
@@ -1,10 +1,8 @@
 const sendSuccessResponse=(res, data)=>{
-  res.header("Access-Control-Allow-Origin", "*");
   return res.status(200).json(data);
   }
 
 const sendErrorResponse=(res, error, statusCode = 500)=>{
-    res.header("Access-Control-Allow-Origin", "*");
     return res.status(statusCode).json(error);
   }
 
