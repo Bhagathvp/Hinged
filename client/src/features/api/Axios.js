3e0d962cfebf87454ec5ca542d92bbf5b2922650
Bhagath Vaishnav P <bhagathvp.ramu@gmail.com>
2023-10-18 19:54:40 +0530

    axios access control changed

--- client/src/features/api/Axios.js
+++ client/src/features/api/Axios.js
@@ -4,9 +4,12 @@ const URL=process.env.NODE_ENV==='production' ? process.env.REACT_APP_SERVER_URL
 console.log(URL)
 const instance = axios.create({
   baseURL:URL,
-  headers: {
-    'Content-Type': 'application/json',
-  }
+
+});
+
+instance.interceptors.request.use((config) => {
+  config.headers['Access-Control-Allow-Origin'] = 'https://hinged.vercel.app';
+  return config;
 });
 
 export default instance;
