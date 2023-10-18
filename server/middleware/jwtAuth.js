bf51f921256d926ab5c7da5390616fff5e07d296
Bhagath Vaishnav P <bhagathvp.ramu@gmail.com>
2023-10-19 00:27:22 +0530

    bug fixing in api

--- server/middleware/jwtAuth.js
+++ server/middleware/jwtAuth.js
@@ -1,9 +1,9 @@
 const jwt = require("jsonwebtoken");
 
 exports.jwtAuth = (req, res, next) => {
-  // const {auth_token} = req.headers;
+  const {Auth_token} = req.headers;
   const token = req.header("auth_token");
-  console.log(token)
+  console.log(token,Auth_token)
   try {
     const id = jwt.verify(auth_token, process.env.ACCESS_TOKEN_SECRET);
     req.userId = id;
