bf4d5903c4e1edb5b37f686f87c6ce34fbc116e3
Bhagath Vaishnav P <bhagathvp.ramu@gmail.com>
2023-10-18 20:34:26 +0530

    cors changed

--- client/src/features/api/Axios.js
+++ client/src/features/api/Axios.js
@@ -4,12 +4,10 @@ const URL=process.env.NODE_ENV==='production' ? process.env.REACT_APP_SERVER_URL
 console.log(URL)
 const instance = axios.create({
   baseURL:URL,
-
-});
-
-instance.interceptors.request.use((config) => {
-  config.headers['Access-Control-Allow-Origin'] = 'https://hinged.vercel.app';
-  return config;
+  
+  headers: {
+    'Content-Type': 'application/json',
+  }
 });
 
 export default instance;
